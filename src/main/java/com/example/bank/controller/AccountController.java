421b2b600ae3c42e68fb887027154647ee207ad4
Gabriel Nogueira <gabrielmvnogueira@gmail.com>
2023-11-17 08:38:43 -0300

    feat: add GET account by id endpoint

--- src/main/java/com/example/bank/controller/AccountController.java
+++ src/main/java/com/example/bank/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.example.bank.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bank.model.dto.AccountCreateRequestDto;
 import com.example.bank.model.dto.AccountCreateResponseDto;
+import com.example.bank.model.dto.AccountDto;
 import com.example.bank.service.AccountService;
 
 @RestController
@@ -25,6 +28,16 @@ public class AccountController {
 			accountService.createAccount(account);
 			return new ResponseEntity<AccountCreateResponseDto>(AccountCreateResponseDto.builder().id("1").build(),
 					HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<String>("", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/{accountId}")
+	public ResponseEntity<?> getAccountById(@PathVariable int accountId) {
+		try {
+			AccountDto data = accountService.getAccountById(accountId);
+			return new ResponseEntity<AccountDto>(data, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<String>("", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
--- src/main/java/com/example/bank/service/AccountService.java
+++ src/main/java/com/example/bank/service/AccountService.java
@@ -1,7 +1,9 @@
 package com.example.bank.service;
 
 import com.example.bank.model.dto.AccountCreateRequestDto;
+import com.example.bank.model.dto.AccountDto;
 
 public interface AccountService {
 	public void createAccount(AccountCreateRequestDto account);
+	public AccountDto getAccountById(int accountId);
 };
